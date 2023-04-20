0b841b27d6a4d1146552d1432070cc45eaf31b18
Derek Cooper <derekcooper20@gmail.com>
2023-04-20 13:13:18 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Sweet Tea and Sunshine Vacations</h1>
+    </div>
+  )
+}
+
+export default App
